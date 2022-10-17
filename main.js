d01303de901108412ccd5cc56434b7f87b234b23
ShenalN <86877221+ShenalN@users.noreply.github.com>
2022-10-17 19:20:49 +0530

    update main.js

--- main.js
+++ main.js
@@ -2,16 +2,6 @@ const listItems = document.querySelectorAll("li");
 const button = document.getElementById("download");
 
 
-for(let i = 0;i < listItems.length;i++) {
-  listItems[i].addEventListener("click",() => {
-    window.scrollTo(0,(window.innerHeight - (innerHeight / 9)) * i)
-  })
-}
-
-button.addEventListener("click",() => {
-  window.scrollTo(0,(window.innerHeight - 180) * 2)
-})
-
 //menu activator
 
 const menuIcon = document.querySelector(".menu")
